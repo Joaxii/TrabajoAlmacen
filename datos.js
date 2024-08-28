bdbb14a2807ff8a3155fec0af1923e861dc28f78
Joaxii <joaquinceballos2007@gmail.com>
2024-08-28 17:14:15 -0300

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,10 @@
+let objetos = [
+    { id: 1, nombre: 'Mandarina', precio: 2, cantidad: 2 },
+    { id: 2, nombre: 'Manzana', precio: 1, cantidad: 7 },
+    { id: 4, nombre: 'Banana', precio: 5, cantidad: 3 },
+    { id: 5, nombre: 'Pera', precio: 10, cantidad: 2 },
+    { id: 6, nombre: 'Uva', precio: 3, cantidad: 1 },
+    { id: 7, nombre: 'Sandia', precio: 10, cantidad: 1 },
+    { id: 8, nombre: 'Vino', precio: 50, cantidad: 1 },
+];
+export default objetos;
